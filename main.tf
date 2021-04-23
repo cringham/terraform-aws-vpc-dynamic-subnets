9060401e87896c12a54ec7f38daad5088749d6e1
cringham <christopher.ringham@gmail.com>
2021-04-23 18:34:53 -0400

    Change tags to be taken as input

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 locals {
-  vpc_netmask         = tonumber(element(split("/", var.vpc_cidr), 1))
-  subnet_mapping_keys = keys(var.subnet_mapping)
+  vpc_netmask = tonumber(element(split("/", var.vpc_cidr), 1))
 }
 
 ######
@@ -9,9 +8,7 @@ locals {
 resource "aws_vpc" "this" {
   cidr_block = var.vpc_cidr
 
-  tags = {
-    Name = "${var.tag_prefix}-vpc-1"
-  }
+  tags = var.vpc_tags
 }
 
 #########
@@ -28,7 +25,5 @@ resource "aws_subnet" "this" {
   cidr_block        = cidrsubnet(aws_vpc.this.cidr_block, each.value.netmask - local.vpc_netmask, each.value.cidr_ordinal)
   availability_zone = data.aws_availability_zones.this.names[each.value.az_ordinal]
 
-  tags = {
-    Name = "${var.tag_prefix}-subnet-${index(local.subnet_mapping_keys, each.key) + 1}"
-  }
+  tags = each.value.tags
 }
